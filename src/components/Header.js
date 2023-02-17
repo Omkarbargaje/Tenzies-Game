08d26057aa06e25f6b2ce6518f6bdfdb7ad80ab3
Omkar <omkarbargaje.22@gmail.com>
2023-02-17 23:31:23 +0530

    added function name in header.js file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function (props) {
+export default function Header(props) {
   return (
     <div className={props.className}>
       <h2>Project By: Omkar Bargaje</h2>
